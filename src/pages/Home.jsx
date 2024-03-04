7fce5c4352131f0284aba9c88535de2f87ad33af
ec221204 <ec221204@qmul.ac.uk>
2024-03-04 16:45:43 +0000

    wip

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react"
 import BigCard from "../components/BigCard.jsx"
 import CardList from "../components/CardList.jsx"
 import GlowCircle from "../components/GlowCircle.jsx"
@@ -5,10 +6,34 @@ import SmallCard from "../components/SmallCard.jsx"
 import WeatherInfo from "../components/WeatherInfo.jsx"
 import StormIcon from "../icons/StormIcon.jsx"
 
+const key = "905f1a7f4bc64c91bb1150432240403"
+const city = "London"
+
 function Home() {
+    const [weatherData, setWeatherData] = useState({})
+
+    const fetchData = () => {
+        fetch(`http://api.weatherapi.com/v1/current.json?q=${city}`, {
+            method: 'GET',
+            headers: {
+                'key': key,
+            }
+        }
+        ).then(res => res.json()).then(res => {
+            console.log(res.current)
+            setWeatherData(res.current)
+        })
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+  
+    
   return (
     <>
         <GlowCircle x={0} y={0} opacity={0.15} blur={60} />
+
 
         <header className="flex w-full h-fit px-8">
             <form className="flex items-center gap-4 w-full">
@@ -22,13 +47,19 @@ function Home() {
             </form>
         </header>
 
+
+
+ 
+
+
+
         <main className="flex items-center flex-col gap-4 w-full h-full overflow-auto px-8 py-8 rounded-[80px]">
-            <WeatherInfo temperature={10} summary={"Mostly clear"} location={"Mile End, United Kingdom"} />
-            <BigCard wind={20} rain={90} humidity={100} />
+            <WeatherInfo temperature={weatherData.temp_c} summary={"Sunny"} location={"London, United Kingdom"} />
+            <BigCard wind={weatherData.wind_kph} rain={weatherData.precip_mm} humidity={weatherData.humidity} />
             <CardList />
-            <BigCard wind={20} rain={90} humidity={100} />
+            <BigCard wind={weatherData.wind_kph} rain={weatherData.precip_mm} humidity={weatherData.humidity} />
             <CardList />
-            <BigCard wind={20} rain={90} humidity={100} />
+            <BigCard wind={weatherData.wind_kph} rain={weatherData.precip_mm} humidity={weatherData.humidity} />
             <CardList />
         </main>
     </>

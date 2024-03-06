695ecd9795f144f44257dc41986245a65a712c6d
conorv <70652079+conorvenus@users.noreply.github.com>
2024-03-06 10:02:10 +0000

    Added skeleton image component

--- src/components/Pin.jsx
+++ src/components/Pin.jsx
@@ -23,7 +23,13 @@ function Pin({ location }) {
     return (
         <div className="bg-dark-gray border border-gray h-full max-h-60 rounded-2xl w-full shadow-primary text-center p-4 grid grid-cols-3 gap-4 items-center">
             <div className="rounded-2xl shadow-primary overflow-hidden h-full">
-                <img className="h-full w-full object-cover" src={image} />
+                {image ? 
+                <img className="h-full w-full object-cover" src={image} /> :
+                <div class="flex items-center justify-center w-full h-full bg-gray-300 rounded bg-dark-gray animate-pulse">
+                    <svg class="w-10 h-10 text-gray" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+                        <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z"/>
+                    </svg>
+                </div>}
             </div>
             <div className="flex flex-col items-start gap-2 col-span-2">
                 <h1 className="text-white font-medium text-left">{location?.name}, <span className="font-normal text-light-gray">{location?.country}</span></h1>

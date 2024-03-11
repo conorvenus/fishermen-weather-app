0fc5cf3744e2dc1f4f086a0f7a72c2dc55a699ec
Conor Venus <conorvenus@gmail.com>
2024-03-11 09:03:11 +0000

    Limited to 3 cards per view

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -4,9 +4,11 @@ function CardList({ title, data }) {
     return (
         <div className="flex flex-col gap-2 text-light-gray w-full">
             <h1>{title}</h1>
-            <section className="grid grid-cols-3 w-full gap-4">
+            <section className="flex w-full overflow-x-auto overflow-y-hidden gap-4 pb-2">
                 {data?.map((item, idx) => (
-                    <SmallCard key={idx} data={item} />   
+                    <div key={idx} className="flex-none w-[calc((100%-2rem)/3)]">
+                        <SmallCard data={item} />
+                    </div>
                 ))}
             </section>
         </div>

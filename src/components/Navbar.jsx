526b4cba4029dea3df0c21adeb01e4b1356e1652
Sandeep <120397149+SaiSVemula@users.noreply.github.com>
2024-03-12 11:42:42 +0000

    Lock Screen added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,39 @@
+import { useState } from 'react'
 import { NavLink, Outlet } from 'react-router-dom'
 
 function Navbar() {
+    const [isLocked, setIsLocked] = useState(false);
+
+    function handleLockClick() {
+        setIsLocked(true)
+    }
+
+    function handleUnlockClick() {
+        setIsLocked(false)
+    }
+
     return (
         <>
+            {isLocked && <div className="fixed inset-0 bg-black opacity-50 z-50"></div>} {/* Overlay when locked */}
             <Outlet />
 
-            <nav className="grid grid-cols-3 gap-8">
+            <nav className="grid grid-cols-3 gap-8 relative">
                 <NavLink to="/" className={({ isActive }) => `flex flex-col items-center justify-center text-light-gray ${isActive ? "nav-selected" : ""}` }>
                     <i className="fa-solid fa-home"></i>
                     Home
                 </NavLink>
-                <button className="bg-blue rounded-full flex justify-center items-center p-4 shadow-primary"><i className="fa-solid fa-lock"></i></button>
+                {isLocked ? (
+                    <button onClick={handleUnlockClick} className="bg-green rounded-full flex justify-center items-center p-4 shadow-primary z-50 relative"><i className="fa-solid fa-unlock"></i></button>
+                ) : (
+                    <button onClick={handleLockClick} className="bg-blue rounded-full flex justify-center items-center p-4 shadow-primary z-50 relative"><i className="fa-solid fa-lock"></i></button>
+                )}
                 <NavLink to="/pins" className={({ isActive }) => `flex flex-col items-center justify-center text-light-gray ${isActive ? "nav-selected" : ""}` }>
                     <i className="fa-solid fa-location-dot"></i>
                     Pins
                 </NavLink>
             </nav>
         </>
-    )
+    );
 }
 
 export default Navbar

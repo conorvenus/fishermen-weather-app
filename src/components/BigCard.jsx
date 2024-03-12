37ae28cf86a26de7e080a53c874004dffe3f221e
conorv <70652079+conorvenus@users.noreply.github.com>
2024-03-12 21:41:44 +0000

    Fixed the errors and made BigCard a more dynamic component

--- src/components/BigCard.jsx
+++ src/components/BigCard.jsx
@@ -1,17 +1,14 @@
-function BigCard({ wind, rain, humidity, data1, data2, data3, m1, m2, m3 }) {
+function BigCard({ title, data }) {
     return (
-        <div className="bg-dark-gray border border-gray rounded-2xl w-full min-h-[150px] shadow-primary grid grid-cols-3 text-center items-center">
-            <div className="h-[60%] w-full flex items-center flex-col justify-center border border-l-0 border-y-0 border-r-light-gray/20">
-                <h1 className="font-bold">{wind} {m1}</h1>
-                <p className="text-light-gray">{data1}</p>
-            </div>
-            <div className="h-[60%] w-full flex items-center flex-col justify-center border border-l-0 border-y-0 border-r-light-gray/20">
-                <h1 className="font-bold">{rain} {m2}</h1>
-                <p className="text-light-gray">{data2}</p>
-            </div>
-            <div className="h-full w-full flex items-center flex-col justify-center">
-                <h1 className="font-bold">{humidity}{m3}</h1>
-                <p className="text-light-gray">{data3}</p>
+        <div className="flex flex-col gap-2 text-light-gray w-full">
+            <h1>{title}</h1>
+            <div className="bg-dark-gray border border-gray rounded-2xl w-full min-h-[150px] shadow-primary grid grid-cols-3 text-center items-center">
+                {data.map((item, idx) => ( 
+                    <div key={idx} className="h-[60%] w-full flex items-center flex-col justify-center border border-l-0 border-y-0 border-r-light-gray/20">
+                        <h1 className="font-bold">{item.value} {item.unit}</h1>
+                        <p className="text-light-gray">{item.description}</p>
+                    </div>
+                ))}
             </div>
         </div>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,61 +5,75 @@ import GlowCircle from "../components/GlowCircle.jsx";
 import WeatherInfo from "../components/WeatherInfo.jsx";
 import { useLocations } from "../hooks/UseLocations.jsx";
 import { getWeatherIcon } from "../utils.jsx";
-import useOpenWeather from "../hooks/useOpenWeather.js";
 
-const key = "905f1a7f4bc64c91bb1150432240403";
+const WEATHER_API_KEY = "905f1a7f4bc64c91bb1150432240403";
+const OPEN_WEATHER_API_KEY = 'c71e8f930b674cc9033f4f2b9d9b7f36';
 
 function Home() {
     const [weatherData, setWeatherData] = useState({});
     const [location, setLocation] = useState('');
     const [suggestions, setSuggestions] = useState([]);
     const { getSelectedLocation, addLocation, selectLocation } = useLocations();
-    const city = 'London'; // example city
-    const { data: openWeatherData, error: weatherError } = useOpenWeather(city);
+    const [openWeatherData, setOpenWeatherData] = useState([]);
+    
+    async function fetchWeatherAPI(loc) {
+        try {
+            const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?q=${loc}&days=7`, {
+                method: 'GET',
+                headers: {
+                    'key': WEATHER_API_KEY,
+                }
+            });
+            const data = await response.json();
+            if (!response.ok) {
+                throw new Error('Weather data fetching failed');
+            }
+            setWeatherData(data);
+            const location = {
+                name: data.location.name,
+                country: data.location.country,
+                selected: false
+            };
+            addLocation(location);
+            selectLocation(location);
+        } catch (error) {
+            console.error('Error fetching weather data:', error);
+        }
+    };
+
+    async function fetchOpenWeatherMap(loc) {
+        try {
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${loc}&appid=${OPEN_WEATHER_API_KEY}&units=metric`);
+            if (!response.ok) {
+                throw new Error('Weather data fetching failed');
+            }
+            const data = await response.json();
+            setOpenWeatherData(data);
+        } catch (err) {
+            console.error('Error fetching weather data:', err);
+        } 
+    }
 
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?q=${city}&days=7`, {
-                    method: 'GET',
-                    headers: {
-                        'key': key,
-                    }
-                });
-                const data = await response.json();
-                if (response.ok) {
-                    setWeatherData(data);
-                    const location = {
-                        name: data.location.name,
-                        country: data.location.country,
-                        selected: false
-                    };
-                    addLocation(location);
-                    selectLocation(location);
-                }
-            } catch (error) {
-                console.error('Error fetching weather data:', error);
-            }
-        };
-        fetchData();
-    }, [city]);
+        fetchWeatherAPI(getSelectedLocation()?.name ?? "London");
+        fetchOpenWeatherMap(getSelectedLocation()?.name ?? "London");
+    }, []);
 
     function handleSubmit(event) {
         event.preventDefault();
-        fetchData(location);
+        fetchWeatherAPI(location);
+        fetchOpenWeatherMap(location);
     }
 
-    // Function to fetch location suggestions
     async function fetchLocationSuggestions(query) {
-        const response = await fetch(`https://api.weatherapi.com/v1/search.json?key=${key}&q=${query}`);
+        const response = await fetch(`https://api.weatherapi.com/v1/search.json?key=${WEATHER_API_KEY}&q=${query}`);
         const data = await response.json();
         setSuggestions(data.map(item => item.name));
     }
 
-    // Function to handle suggestion click
     function handleSuggestionClick(suggestion) {
         setLocation(suggestion);
-        setSuggestions([]); // Clear suggestions
+        setSuggestions([]);
     }
     
     return (
@@ -94,13 +108,25 @@ function Home() {
             </header>
 
         <main className="flex items-center flex-col gap-4 w-full h-full overflow-auto px-8 py-8 rounded-[80px]">
-            {weatherError && <div>Error fetching weather data: {weatherError}</div>}
-            {!weatherError && Object.keys(weatherData).length > 0 && (
+            {weatherData && (
                 <>
                     <WeatherInfo temperature={weatherData?.current?.temp_c} summary={weatherData?.current?.condition?.text} location={weatherData?.location} icon={getWeatherIcon(weatherData?.current?.condition?.code)} />
-                    <BigCard wind={weatherData?.current?.wind_kph} rain={weatherData?.current?.precip_mm} humidity={weatherData?.current?.humidity} data1={'wind'}
-                    data2 = {'Rain'} data3 = {'Humidity'} m1 = {'km/h'} m2 = {'mm'} m3 = {'%'}/>
-                    <BigCard wind={openWeatherData?.main?.pressure} rain={openWeatherData?.main?.pressure} humidity={openWeatherData?.visibility} data1={'Pressure (sea level)'} m1={"Pa"} data2={'Feels like'} m2 = {'Fa'} data3 = {'Visibility'} m3 = {'m'}/>
+                    <BigCard 
+                        title={"Current"}
+                        data={[
+                            { value: weatherData?.current?.wind_kph, unit: 'km/h', description: 'Wind' },
+                            { value: weatherData?.current?.precip_mm, unit: 'mm', description: 'Rain' },
+                            { value: weatherData?.current?.humidity, unit: '%', description: 'Humidity' }
+                        ]}
+                     />
+                    <BigCard 
+                        title={"Marine"}
+                        data={[
+                            { value: openWeatherData?.main?.pressure, unit: 'Pa', description: 'Pressure' },
+                            { value: openWeatherData?.main?.feels_like, unit: '°C', description: 'Feels Like' },
+                            { value: openWeatherData?.visibility, unit: 'm', description: 'Visibility' }  
+                        ]}
+                    />
                     <CardList title={"Hourly"} data={weatherData?.forecast?.forecastday[0].hour} />
                     <CardList title={"Daily"} data={weatherData?.forecast?.forecastday} />
                 </>

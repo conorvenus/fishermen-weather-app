defa4a11b36732b9d303b99fa4cbbbe6e09b7573
DESKTOP-IUO4VGF\secretly <mvartanian@datrics.ai>
2024-03-17 07:06:22 +0000

    Pins Caching

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 })
+
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     VitePWA({
+//       registerType: 'autoUpdate',
+//       includeAssets: [],
+//       manifest: {
+//         name: 'Fishermen Weather App',
+//         short_name: 'FishermenWeather',
+//         description: 'Weather App for fishermen',
+//         theme_color: '#ffffff',
+//         start_url: '/',
+//         display: 'standalone',
+//         background_color: '#ffffff',
+//       },
+//     }),
+//   ],
+// });

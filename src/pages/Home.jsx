b8a6614e29d9210159da38882f7d5db200ad6136
Conor Venus <conorvenus@gmail.com>
2024-03-18 11:09:32 +0000

    Fixed location button submitting search bar also, causing an override

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -108,6 +108,7 @@ function Home() {
 
     const tidalChartRef = useRef(null);
     const waveChartRef = useRef(null);
+
     useEffect(() => {
         if (coordinates.latitude != null && coordinates.longitude != null) {
             fetchTidalData();
@@ -267,7 +268,7 @@ function Home() {
 
             <header className="flex w-full h-fit px-8">
                 <form className="flex items-center gap-4 w-full max-w-2xl mx-auto" onSubmit={handleSubmit}>
-                    <button onClick={refreshWeatherData} className="bg-blue pulsing-btn rounded-full flex justify-center items-center h-full aspect-square shadow-primary">
+                    <button type="button" onClick={refreshWeatherData} className="bg-blue pulsing-btn rounded-full flex justify-center items-center h-full aspect-square shadow-primary">
                         <i className="fas fa-map-marker-alt"></i>
                     </button>
                     <div className="flex items-center gap-4 bg-dark-gray border border-gray rounded-2xl w-full py-2 px-4 shadow-primary relative">

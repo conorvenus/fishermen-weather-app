9a507cf0396f40a5be64e586cdd9703ea766e2ce
conorv <70652079+conorvenus@users.noreply.github.com>
2024-03-20 20:23:35 +0000

    Offline cache v6

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 // on service worker install, cache the home and pins pages
 self.addEventListener('install', function(event) {
     event.waitUntil(async function() {
-        const cache = await caches.open('v5');
+        const cache = await caches.open('v6');
         await cache.addAll([
             '/',
             // '/pins',
@@ -15,7 +15,7 @@ self.addEventListener('fetch', function(event) {
     if (event.request.url.includes(self.location.origin) || event.request.url.includes('cdnjs')) {
         event.respondWith(async function() {
             // try to fetch from v1 cache
-            const cache = await caches.open('v5');
+            const cache = await caches.open('v6');
             const cachedResponse = await cache.match(event.request);
 
             // if it is cached, return it

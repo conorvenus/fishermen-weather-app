66704b77cee1637c725b461154b546cffea2b2de
Conor Venus <conorvenus@gmail.com>
2024-03-20 17:24:13 +0000

    Changed cache version

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 // on service worker install, cache the home and pins pages
 self.addEventListener('install', function(event) {
     event.waitUntil(async function() {
-        const cache = await caches.open('v1');
+        const cache = await caches.open('v2');
         await cache.addAll([
             '/',
             // '/pins',
@@ -16,7 +16,7 @@ self.addEventListener('fetch', function(event) {
     if (event.request.url.includes(self.location.origin) || event.request.url.includes('cdnjs')) {
         event.respondWith(async function() {
             // try to fetch from v1 cache
-            const cache = await caches.open('v1');
+            const cache = await caches.open('v2');
             const cachedResponse = await cache.match(event.request);
 
             // if it is cached, return it

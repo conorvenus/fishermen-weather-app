e8a965d74fafde2904a7d3c9a493b35d64074480
conorv <70652079+conorvenus@users.noreply.github.com>
2024-03-21 21:15:29 +0000

    Fixed light/dark mode for pins

--- src/components/Pin.jsx
+++ src/components/Pin.jsx
@@ -48,7 +48,7 @@ function Pin({ location, delay }) {
             <div className="rounded-2xl shadow-primary overflow-hidden h-full">
                 {image ? 
                 <img className="h-full w-full object-cover" src={image} /> :
-                <div className="flex items-center justify-center w-full h-full bg-gray-300 rounded bg-dark-gray animate-pulse">
+                <div className="flex items-center justify-center w-full h-full bg-gray-300 rounded dark:bg-dark-gray bg-light-gray animate-pulse">
                     <svg className="w-10 h-10 text-gray" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
                         <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z"/>
                     </svg>
@@ -57,24 +57,24 @@ function Pin({ location, delay }) {
             <div className="flex flex-col items-start gap-2 col-span-2">
                 <h1 className="dark:text-white text-black font-medium text-left">{location?.name}, <span className="font-normal dark:text-light-gray text-gray">{location?.country}</span></h1>
                 <div className="flex items-center gap-2">
-                    <div className="flex shadow-primary items-center gap-2 bg-gray rounded-lg px-2 py-1 text-xs">
+                    <div className="flex shadow-primary items-center gap-2 dark:bg-gray bg-light-gray text-white rounded-lg px-2 py-1 text-xs">
                         <i className="fa-solid fa-download"></i>
                         <p>{capitalizeEachWord(format(location?.lastUpdated))}</p>
                     </div>
                 </div>
                 <div className="flex items-center gap-2">
                     {location.selected ? (
-                        <button disabled className="bg-gray rounded-lg px-2 py-1 shadow-primary font-bold flex items-center gap-2 text-xs">
+                        <button disabled className="dark:bg-gray bg-light-graytext-white rounded-lg px-2 py-1 shadow-primary font-bold flex items-center gap-2 text-xs">
                             <i className="fa-solid fa-eye-slash"></i>
                             Viewing
                         </button>
                     ) : (
-                        <button onClick={viewLocation} className="bg-blue rounded-lg px-2 py-1 shadow-primary font-bold flex items-center gap-2 text-xs">
+                        <button onClick={viewLocation} className="bg-blue text-white rounded-lg px-2 py-1 shadow-primary font-bold flex items-center gap-2 text-xs">
                             <i className="fa-solid fa-eye"></i>
                             View
                         </button>
                     )}
-                    <button onClick={() => removeLocation(location)} className="bg-gray rounded-lg px-2 py-1 shadow-primary font-bold flex items-center gap-2 text-xs">
+                    <button onClick={() => removeLocation(location)} className="dark:bg-gray bg-light-gray text-white rounded-lg px-2 py-1 shadow-primary font-bold flex items-center gap-2 text-xs">
                         <i className="fa-solid fa-trash"></i>
                         Remove
                     </button>

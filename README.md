e89d88a977a2490f9401df28ef932109b2159237
conorv <70652079+conorvenus@users.noreply.github.com>
2024-03-21 21:43:33 +0000

    Updated README, credited image placeholder

--- README.md
+++ README.md
@@ -1,8 +1,24 @@
-# React + Vite
+# 🐟 FishCast
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+FishCast is a weather app for fishermen, designed to help them plan their fishing journeys safely and offline out at sea.
 
-Currently, two official plugins are available:
+**Note:** Offline mode will only work if you build the application for production, please follow the second guide.
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+## Usage
+
+1. **Clone the Repository**: 
+   After cloning the repository, navigate to the root directory in your terminal.
+
+2. **Install Dependencies**: 
+   Run `npm i` or `npm install` in the directory.
+
+3. **Start the Application**: 
+   Execute `npm run dev` from the directory.
+
+## Usage with Offline Mode
+
+1. **Build the Application**:
+    Run `npm run build` from the directory, this will build the files for production and include the offline features (as they are designed only for production).
+
+2. **Run the Build**:
+    Run `npm run preview` from the directory after building to serve the built application in production preview mode, you will be able to test offline mode.
--- src/components/Pin.jsx
+++ src/components/Pin.jsx
@@ -47,12 +47,14 @@ function Pin({ location, delay }) {
         <motion.div initial={{y: -100, opacity: 0}} animate={{y: 0, opacity: 1}} transition={{duration: 1, delay}} className="dark:bg-dark-gray border dark:border-gray bg-white border-white h-full max-h-60 rounded-2xl w-full shadow-primary text-center p-4 grid grid-cols-3 gap-4 items-center">
             <div className="rounded-2xl shadow-primary overflow-hidden h-full">
                 {image ? 
-                <img className="h-full w-full object-cover" src={image} /> :
-                <div className="flex items-center justify-center w-full h-full bg-gray-300 rounded dark:bg-dark-gray bg-light-gray animate-pulse">
-                    <svg className="w-10 h-10 text-gray" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
-                        <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z"/>
-                    </svg>
-                </div>}
+                <img className="h-full w-full object-cover" src={image} /> : (
+                    // Image placeholder taken and modified from https://flowbite.com/docs/components/skeleton/
+                    <div className="flex items-center justify-center w-full h-full bg-gray-300 rounded dark:bg-dark-gray bg-light-gray animate-pulse">
+                        <svg className="w-10 h-10 text-gray" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+                            <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z"/>
+                        </svg>
+                    </div>
+                )}
             </div>
             <div className="flex flex-col items-start gap-2 col-span-2">
                 <h1 className="dark:text-white text-black font-medium text-left">{location?.name}, <span className="font-normal dark:text-light-gray text-gray">{location?.country}</span></h1>
